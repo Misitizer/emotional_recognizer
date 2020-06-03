50954d2e3306c999f4ddfd0514584adaeb10b8a4
unknown <Mrkirilldav@yandex.ru>
2020-06-03 22:58:33 +0300

    add telebot changes

--- scr/main.py
+++ scr/main.py
@@ -1,38 +1,70 @@
-#from keras.models import load_model
+from tensorflow.keras.models import load_model
 import recognize_face, reshape_photo
 import cv2
-# from keras import backend as K
-# K.common.set_image_dim_ordering('th')
+from keras import backend as K
+K.common.set_image_dim_ordering('th')
+from matplotlib import pyplot as plt
+import numpy as np
+import telebot
 
 def swish_activation(x):
     return (K.sigmoid(x) * x)
 
-image_path = r'D:\diplom\images\Nastya4.jpg'
-image = cv2.imread(image_path)
+bot = telebot.TeleBot('1136311061:AAG2gVnnJTCpN6PNX6aDRaov4pOWpCSWzjo')
 
-cropped = recognize_face.cropFace(image)
-recognize_face.save_faces(cropped)
-reshape_photo.scale_image(input_image_path = r'D:\diplom\faces\1.jpg',
-             output_image_path = r'D:\diplom\faces\1.jpg',
-             width=48,
-             height= 48)
+telebot.apihelper.proxy = {'https':'163.172.189.32:8811'}
 
-face_path = r'D:\diplom\faces\1.jpg'
-face = cv2.imread(face_path, 0)
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Отправь свое фото")
 
-f = face.reshape(1, 48, 48, 1)
+@bot.message_handler(content_types=['photo'])
+def handle_docs_document(message):
 
-reloaded = load_model('emotional_model.h5', custom_objects={'swish_activation': swish_activation})
+    def swish_activation(x):
+        return (K.sigmoid(x) * x)
 
-emotion = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
-predict = reloaded.predict(f)
-print(emotion[np.argmax(predict)])
+    file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    scr = r'C:\Users\kdavydov\PycharmProjects\untitled\image\1.jpg'
+
+    with open(scr, 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+    bot.reply_to(message, 'Фото добавлено')
+
+    image_path = r'C:\Users\kdavydov\PycharmProjects\untitled\image\1.jpg'
+    image = cv2.imread(image_path)
+    cropped = recognize_face.cropFace(image)
+    recognize_face.save_faces(cropped)
+
+    face_path = r'C:\Users\kdavydov\PycharmProjects\untitled\faces\1.jpg'
+    face = open(face_path, 'rb')
+
+    bot.send_message(chat_id= message.chat.id, text='Смотрите кого нашел')
+    bot.send_photo(chat_id = message.chat.id, photo= face)
+
+    reshape_photo.scale_image(input_image_path = r'C:\Users\kdavydov\PycharmProjects\untitled\faces\1.jpg',
+                output_image_path = r'C:\Users\kdavydov\PycharmProjects\untitled\faces\1.jpg',
+                width = 48,
+                height = 48)
+    bot.send_message(chat_id=message.chat.id, text = 'Пытаюсь понять эмоцию')
+    face_path = r'C:\Users\kdavydov\PycharmProjects\untitled\faces\1.jpg'
+    face = cv2.imread(face_path, 0)
+    f = face.reshape(1, 48, 48, 1)
+
+    reloaded = load_model(r'C:\Users\kdavydov\PycharmProjects\untitled\scr\emotional_model.h5', custom_objects={'swish_activation': swish_activation})
+
+    emotion = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+    predict = reloaded.predict(f)
+
+    names = np.arange(7)
+    plt.bar(names, height=predict[0])
+    plt.xticks(names, labels=emotion)
+    plt.savefig(r'C:\Users\kdavydov\PycharmProjects\untitled\faces\2.jpg')
+    bot.send_message(chat_id=message.chat.id, text = emotion[np.argmax(predict)])
+    plot = open(r'C:\Users\kdavydov\PycharmProjects\untitled\faces\2.jpg','rb')
+    bot.send_photo(chat_id=message.chat.id, photo=plot)
 
 
-fig, (ax1, ax2) = plt.subplots(1,2)
-
-names = np.arange(7)
-ax1.bar(names, height=predict[0]*100)
-ax1.set_xticks(names, emotion)
-ax2.imshow(face.reshape((48,48)), interpolation = 'none', cmap = 'gray')
-plt.show()
+bot.polling()
